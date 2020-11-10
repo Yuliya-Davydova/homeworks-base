8f19639ec3255caf2b2d11fe58f99e74de58a474
Yuliya Davydova <vorstellung7@gmail.com>
2020-11-11 02:56:02 +0300

    Add Task1 test

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,14 +1,49 @@
 package org.example;
 
-/**
- * Hello world!
- */
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class App {
-    public static int max(int a, int b) {
-        return Math.max(a, b);
-    }
 
     public static void main(String[] args) {
-        System.out.println("Hello World.");
+        System.out.println("Введите размер массива  MxN");
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int m = 0;
+        int n = 0;
+
+        if (scanner.hasNextInt()) {
+            n = scanner.nextInt();
+            m = scanner.nextInt();
+        }
+        if (n == m) {
+            int mass[][] = new int[n][m];
+            for (int i = 0; i < n; i++) { //Заполнение массива случайными числами от 0 до 50
+                for (int j = 0; j < m; j++) {
+                    mass[i][j] = random.nextInt(9);
+                }
+            }
+            for (int i = 0; i < n; i++) {// вывод массива в консоль
+                for (int j = 0; j < m; j++) {
+                    System.out.print(mass[i][j] + " ");
+                }
+                System.out.println();
+            }
+            for (int i = 0; i < n; i++) {// вывод диагонали массива в консоль
+                for (int j = 0; j < m; j++) {
+                    if (mass[i] == mass[j]) {
+                        System.out.print(mass[i][j]);
+                    }
+                }
+            }
+            for (int i = n - 1; i < n; i++) {
+                for (int j = m - 1 - i; j >= 0; j--) {
+                    System.out.print(mass[i][j]);
+                }
+            }
+        } else {
+            System.out.println("Ошибка! Введите числа, чтобы получилась квадратная матрица: M должно равняться N");
+        }
     }
 }
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -8,21 +8,4 @@ import static org.junit.jupiter.api.Assertions.*;
  * Unit test for simple App.
  */
 public class AppTest {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void zeroIsLessThenOne() {
-        int max = App.max(1, 0); // вызов класса
-        assertEquals(1, max);
-
-    }
-
-    @Test
-    public void zeroIsLessThenOneReversed() {
-        int max = App.max(0, 1); // вызов класса
-        assertEquals(1, max);
-
-
-    }
 }
