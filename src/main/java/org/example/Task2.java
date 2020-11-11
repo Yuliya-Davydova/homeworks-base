26a63410f73f9cbd2aaa440da014530b14ea651f
Yuliya Davydova <vorstellung7@gmail.com>
2020-11-11 16:04:32 +0300

    Add Task1 test

--- src/main/java/org/example/Task2.java
+++ src/main/java/org/example/Task2.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 
 public class Task2 {
+
     public static void main(String[] args) {
         System.out.println("Введите размер массива  MxN");
         Scanner scanner = new Scanner(System.in);
@@ -18,14 +19,30 @@ public class Task2 {
         }
         if (n == m) {
             int mass[][] = new int[n][m];
-            for (int i = 0; i < n; i++) { //Заполнение массива случайными числами от 0 до 50
+            for (int i = 0; i < n; i++) { //Заполнение массива случайными числами от 0 до 9
                 for (int j = 0; j < m; j++) {
                     mass[i][j] = random.nextInt(9);
                 }
             }
+            System.out.println("Вывод массива:");
             for (int i = 0; i < n; i++) {// вывод массива в консоль
                 for (int j = 0; j < m; j++) {
                     System.out.print(mass[i][j] + " ");
+                }
+                System.out.println();
+            }
+            for (int i = 0; i < n; i++) {// вывод массива в консоль
+                for (int j = 0; j < m; j++) {
+                    int transp = mass[i][j];
+                    mass[i][j] = mass[j][i];
+                    mass[j][i] = transp;
+                }
+            }
+            System.out.println();
+            System.out.println("Новая матрица");
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    System.out.print(mass[j][i] + " ");
                 }
                 System.out.println();
             }
