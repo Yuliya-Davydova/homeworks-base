324f354764e3b41505eda40e4a5632887035b7d2
Yuliya Davydova <vorstellung7@gmail.com>
2020-11-11 15:19:23 +0300

    Add Task1 test

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -21,7 +21,7 @@ public class App {
             int mass[][] = new int[n][m];
             for (int i = 0; i < n; i++) { //Заполнение массива случайными числами от 0 до 50
                 for (int j = 0; j < m; j++) {
-                    mass[i][j] = random.nextInt(9);
+                    mass[i][j] = random.nextInt(50);
                 }
             }
             for (int i = 0; i < n; i++) {// вывод массива в консоль
@@ -39,13 +39,15 @@ public class App {
             }
             for (int i = n - 1; i < n; i++) { // вывод чисел под диагональю, оказалось выводит только нижний левый угол(((
                 for (int j = m - 1 - i; j >= 0; j--) {
-                    System.out.print(mass[i][j]);
+                    System.out.print(mass[i][j] + " ");
                 }
             }
             System.out.println();
             for (int i = 0; i < n; i++) { // вывод главной диагонали и всех чисел под диагональю
                 for (int j = 0; j <= i; j++) {
-                    System.out.print(mass[i][j] + " ");
+                    if (mass[i][j] % 2 == 1) {
+                        System.out.print(mass[i][j] + " ");
+                    }
                 }
                 System.out.println();
             }
---
+++ src/main/java/org/example/Task2.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class Task2 {
+    public static void main(String[] args) {
+        System.out.println("Введите размер массива  MxN");
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int m = 0;
+        int n = 0;
+
+        if (scanner.hasNextInt()) {
+            n = scanner.nextInt();
+            m = scanner.nextInt();
+        }
+        if (n == m) {
+            int mass[][] = new int[n][m];
+            for (int i = 0; i < n; i++) { //Заполнение массива случайными числами от 0 до 50
+                for (int j = 0; j < m; j++) {
+                    mass[i][j] = random.nextInt(9);
+                }
+            }
+            for (int i = 0; i < n; i++) {// вывод массива в консоль
+                for (int j = 0; j < m; j++) {
+                    System.out.print(mass[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
