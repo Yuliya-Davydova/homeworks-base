1d7a58bd59ed8732c01cec4bd8a37a39e4445fdd
Yuliya Davydova <vorstellung7@gmail.com>
2020-11-12 19:27:52 +0300

    Add Task1 test

---
+++ src/main/java/org/example/nClas.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class nClas {
+    public static void main(String[] args) {
+        int[][] input = new int[3][3];
+        // ввод данных в массив
+        int result = calcSometh(input);
+        System.out.println(result);
+    }
+
+    public static int calcSometh(int[][] array) {
+        // работа с for и прочее
+        return 1;
+    }
+}
