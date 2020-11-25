b4368841ef9c4799fd86ec8a455bbebe01d98b26
Yuliya Davydova <vorstellung7@gmail.com>
2020-11-26 02:28:28 +0300

    Edit name Samsumg for Samsung

--- src/main/java/FileSystem/App.java
+++ src/main/java/FileSystem/App.java
@@ -3,8 +3,27 @@ package FileSystem;
 import java.util.Scanner;
 
 public class App {
+    static Folder tree;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String result = scanner.nextLine();
+            if (result == "print") {
+                printTree();
+            } else {
+                addTree(result);
+            }
+        }
+
+    }
+
+    static void printTree() {
+        // TODO: 11/26/20 Вывод дерева
+        System.out.println("Здесь должен быть вывод дерева");
+    }
+
+    static void addTree(String stringTree) {
 
     }
 }
--- src/main/java/FileSystem/Folder.java
+++ src/main/java/FileSystem/Folder.java
@@ -1,18 +1,22 @@
 package FileSystem;
 
+import java.util.ArrayList;
+
 public class Folder {
     private String name;
+    ArrayList<Folder> folders = new ArrayList<>();
+    ArrayList<File> files = new ArrayList<>();
 
     public String name() {
         return name;
     }
 
     public void add(File file) {
-
+        files.add(file);
     }
 
     public void add(Folder folder) {
-
+        folders.add(folder);
     }
 
 }
